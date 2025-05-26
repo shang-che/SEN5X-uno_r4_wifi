ce35c405c4b587a3506ff999f9e2efabad6bbd4f
shang-che <49426336+shang-che@users.noreply.github.com>
2025-05-26 16:48:02 +0200

    a template about config

---
+++ src/BlynkConfig.h
@@ -0,0 +1,14 @@
+#ifndef BLYNK_CONFIG_H
+#define BLYNK_CONFIG_H
+// set your Blynk template ID, template name, and auth token here
+#define BLYNK_TEMPLATE_ID "YOUR_TEMPLATE_ID"
+#define BLYNK_TEMPLATE_NAME "YOUR_TEMPLATE_NAME"
+#define BLYNK_AUTH_TOKEN "YOUR_BLYNK_AUTH_TOKEN"
+
+#define BLYNK_WIFI_SSID "SSID_NAME"           // Replace with your WiFi SSID
+#define BLYNK_WIFI_PASS "YOUR_WIFI_PASSWORD"  // Replace with your WiFi password
+
+char blynk_ssid[] = BLYNK_WIFI_SSID;
+char blynk_pass[] = BLYNK_WIFI_PASS;
+
+#endif  // BLYNK_CONFIG_H
